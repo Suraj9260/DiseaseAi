4bc3ffb7b1fffac35059b6d368773fcc06a78a55
Suraj Kumar Chaurasiya <141571860+Suraj9260@users.noreply.github.com>
2025-03-30 19:47:02 +0530

    gemini.py

--- gemini.py
+++ gemini.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 
 
-os.environ["API_KEY"] = "AIzaSyAaALLmpDW7wRne7nuHtEbeXOAewlTAk9s"
+os.environ["API_KEY"] = ""
 genai.configure(api_key=os.environ["API_KEY"])
 
 model1 = genai.GenerativeModel('gemini-1.5-flash')
